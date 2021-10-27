1d4ed62cb3ff1e64d790e9e45054fb719ac2ebb1
Lucas Mendonça <lucas21rio@gmail.com>
2021-10-26 22:05:00 -0300

    link de imagem

--- vercel/src/App.js
+++ vercel/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import imagem from './components/homem-feliz-dinheiro.jpg'
+// import imagem from ''
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={imagem}/>
+        <img src={"https://raw.githubusercontent.com/rquadrosDH/imagens/main/homem-feliz-dinheiro.jpg"}/>
         <p>
           O primeiro milhão a gente nunca esquece.
         </p>
