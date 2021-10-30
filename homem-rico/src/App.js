88aa200924231d205ee814211b77d0f24dfe8d5f
Lucas Mendonça <lucas21rio@gmail.com>
2021-10-30 16:38:02 -0300

    props e children

--- homem-rico/src/App.js
+++ homem-rico/src/App.js
@@ -1,8 +1,10 @@
+
 import './App.css';
-// import imagem from ''
+
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={"https://raw.githubusercontent.com/rquadrosDH/imagens/main/homem-feliz-dinheiro.jpg"} alt="homem rico e feliz"/>
@@ -11,7 +13,8 @@ function App() {
         </p>
       </header>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
