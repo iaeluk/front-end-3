9761ad79be16c593371f7c433bd81bec105f18a7
Lucas Mendonça <lucas21rio@gmail.com>
2021-11-02 19:23:25 -0300

    aula 06

--- convidados/src/index.js
+++ convidados/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-
+import './styles/index.css';
+import App from './container/App';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +9,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-
 
